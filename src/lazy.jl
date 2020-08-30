50f2a497b1d73fc9d79e7f874aa7b1d73209ad40
Riccardo Cioffi <ric.cioffi90@gmail.com>
2020-08-30 16:17:14 +0200

    add lazy recursive version
    Signed-off-by: Riccardo Cioffi <ric.cioffi90@gmail.com>

--- src/lazy.jl
+++ src/lazy.jl
@@ -3,7 +3,11 @@ using Lazy
 # TODO: (1) Try to make recursive
 
 function lazy_half_grid(n::Int, α)
-    n -= 1
-    baseline = 0.5*(α - 1)/(α^n - 1)
-    grid = @>> Lazy.range() map(x -> baseline*sum(α^(j - 1) for j = 1:x)) prepend(0)
+    baseline = 0.5*(α - 1)/(α^(n - 1) - 1)
+    return @>> Lazy.range() map(x -> baseline*sum(α^(j - 1) for j = 1:x)) prepend(0) take(n)
+end
+
+function lazy_half_grid_recursive(n::Int, α)
+    baseline = 0.5*(α - 1)/(α^(n - 1) - 1)
+    return @>> iterated(x -> baseline + α*x, baseline) prepend(0) take(n)
 end
